1364b87f9c6c7421d2ac9685d15ea1322987e8e0
a Boelghet <aboelghet@Ahmads-MacBook-Air.local>
2017-02-23 15:02:18 +0200

    Initial Commit

---
+++ CubeTask2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CubeTask2
+//
+//  Created by Ahmad Aboelghet on 2/23/17.
+//  Copyright © 2017 Ahmad Aboelghet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
